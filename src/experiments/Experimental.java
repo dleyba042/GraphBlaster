0532bd535aeaebabaae4b60256791db974976dca
Devin Leyba <dleyba042@gmail.com>
2022-11-27 17:36:46 -0800

    struggling to consitently draw using my row column approach

---
+++ src/experiments/Experimental.java
@@ -0,0 +1,44 @@
+package experiments;
+
+import javafx.scene.Group;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+
+
+public class Experimental
+{
+
+    public static Scene devinTry()
+    {
+        Group root = new Group();
+
+        for(int x = 0 ; x<400; x+= 20)
+        {
+            for(int y = 0; y<400; y+= 20)
+            {
+                Node myNode = new MyNode(x, y, 20, 20);
+                root.getChildren().add(myNode);
+            }
+        }
+        return new Scene(root, 400,400);
+    }
+
+    public static class MyNode extends StackPane
+    {
+        public MyNode(double x, double y, double width, double height)
+        {
+            Rectangle rec =new Rectangle(width,height);
+            rec.setStroke(Color.BLACK);
+            rec.setFill(Color.ORANGE);
+
+            setTranslateX(x);
+            setTranslateY(y);
+
+            getChildren().addAll(rec);
+        }
+
+    }
+}
--- src/main/Tester.java
+++ src/main/Tester.java
@@ -3,31 +3,23 @@ package main;
 import ds.MazeGraph;
 import javafx.application.Application;
 import javafx.geometry.Insets;
-import javafx.scene.Group;
-import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Line;
-import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class Tester extends Application
 {
 
     Stage window;
     private static final int WINDOW_PADDING = 20;
-    private static final int HEIGHT_ADJUSTER = 80;
-
     public static void main(String[] args)
     {
         launch(args);
         //Looks like we only have to do a good union up to the last one
         //probably because once we've gotten to it, it has no valid options left
-        //TODO test the maze graph here -> seems to work well enough
+
     }
 
     @Override
@@ -35,14 +27,21 @@ public class Tester extends Application
     {
         //Create original graph
 
+        int rows = 50;
+        int cols = 50;
+
         MazeGraph graph = new MazeGraph(4,4);
+
+        int idealWidth =  ((rows * 10) % 800 > 0) ? 800 : 600;
+        int idealHeight = ((rows * 10) % 800 > 0) ? 800 : 600;
+
         window = primaryStage;
         window.setTitle("Devin's Badass Graph");
-        window.setScene(fromGrid(graph,7,7, 600,600));
+        window.setScene(fromGrid(graph,rows,cols, idealHeight,idealWidth));
         window.show();
     }
 
-    public static class MyLine extends Group
+    public static class MyLine extends TilePane
     {
         public  MyLine(double startX, double startY, double endX, double endY, double translateX, double translateY, boolean knockindDown)
         {
@@ -58,54 +57,25 @@ public class Tester extends Application
     }
 
 
-
-    public static class MyNode extends StackPane
-    {
-        public MyNode(double x, double y, double width, double height)
-        {
-            Rectangle rec =new Rectangle(width,height);
-            rec.setStroke(Color.BLACK);
-            rec.setFill(Color.ORANGE);
-
-            setTranslateX(x);
-            setTranslateY(y);
-
-            getChildren().addAll(rec);
-        }
-
-    }
-
-    public static Scene devinTry()
-    {
-        Group root = new Group();
-
-        for(int x = 0 ; x<400; x+= 20)
-        {
-            for(int y = 0; y<400; y+= 20)
-            {
-                Node myNode = new MyNode(x, y, 20, 20);
-                root.getChildren().add(myNode);
-            }
-        }
-        return new Scene(root, 400,400);
-    }
-
     public static Scene fromGrid(MazeGraph graph,int rows, int cols, int height, int width)
     {
         GridPane gridPane = new GridPane();
         gridPane.setPadding(new Insets(WINDOW_PADDING,WINDOW_PADDING,WINDOW_PADDING,WINDOW_PADDING));
-        gridPane.setVgap(10);
-        gridPane.setHgap(10);
+        gridPane.setVgap(0);
+        gridPane.setHgap(0);
 
-
-        int leftCol = 0;
+        /*
+        //TODO may be usefull later
         int widthCol = (width - (WINDOW_PADDING * 2)) / cols;
         int heightRow = (height - (WINDOW_PADDING * 2) - HEIGHT_ADJUSTER) / rows;
         int bottomRow = heightRow * (rows - 1);
 
-        System.out.println("Width = " + widthCol);
-        System.out.println("Height = " + heightRow);
+         */
 
+       // System.out.println("Width = " + widthCol);
+       // System.out.println("Height = " + heightRow);
+
+        /*
         //fill in the rest
         for(int x = leftCol ; x<width-heightRow; x+= widthCol)
         {
@@ -113,24 +83,34 @@ public class Tester extends Application
             {
                 gridPane.getChildren().addAll(drawGrid(x, y,leftCol,bottomRow, widthCol,heightRow));
             }
+
         }
 
+         */
+
+        int wholeRowWidth = width - (WINDOW_PADDING * 2) - 20;
+        int wholeColHeight = height - (WINDOW_PADDING * 2) -20 ;
+        int widthCol = wholeRowWidth / cols ;
+        int heightRow = wholeColHeight / rows;
+
+        drawGrid(0,wholeRowWidth,wholeColHeight ,widthCol,heightRow,gridPane,rows);
+
+
         //Testing this now need to parse in graph
-        knockDownWalls(true,true,true,true,widthCol * 2, heightRow * 3,gridPane,widthCol,heightRow);
+        //knockDownWalls(true,true,true,true,widthCol * 2, heightRow * 3,gridPane,widthCol,heightRow);
 
 
         return new Scene(gridPane,width,height);
 
     }
 
+
     /**
      * Weird thing about the translateY for the sides of the square is that it seems to start in the middle and
      * spread out that's why the translate y is 10 apart
-     * @param xCoord
-     * @param yCoord
      * @return
      */
-
+/*
     public static List<Node> drawGrid(int xCoord, int yCoord,int leftCol, int bottomRow, int width, int height)
     {
             List<Node> square = new ArrayList<>();
@@ -150,6 +130,9 @@ public class Tester extends Application
 
         return square;
     }
+
+ */
+
 
     public static void knockDownWalls(boolean left, boolean right, boolean top, boolean bottom,
                                       double translateX, double translateY, GridPane graph, int wallWidth, int wallHeight)
@@ -171,6 +154,19 @@ public class Tester extends Application
         {
             graph.getChildren().add(new MyLine(0,0,0,wallHeight,translateX + wallWidth,translateY + (wallHeight/2),true));
         }
+    }
+
+    public static void drawGrid(int leftCol, int wholeRowWidth, int wholeColHeight,int widthCol, int heightRow, GridPane gridPane, int numRows)
+    {
+        int counter = 0;
+
+        for(int x = leftCol, y=0 ; numRows >= counter; x+= widthCol, y+= heightRow)
+        {
+            gridPane.getChildren().addAll(new MyLine(x, 0,x,wholeColHeight,x,0,false));
+            gridPane.getChildren().addAll(new MyLine(0, y ,wholeRowWidth,y,0,y,false));
+            counter++;
+        }
+        gridPane.getChildren().addAll(new MyLine(0, wholeColHeight,0,wholeColHeight,wholeRowWidth,wholeColHeight,false));
     }
 
 }
